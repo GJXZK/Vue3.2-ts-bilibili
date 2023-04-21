771093427bfe6694ef1574144b16b80434394d6a
GJXZK <2587579585@qq.com>
2023-04-21 10:32:22 +0800

    默认

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <h1></h1>
-  <!-- aaa -->
+  <h1>哔哩哔哩主页</h1>
+  <router-link to="/video/1">点我去视频详情页</router-link>
 </template>
 
 <script>
 export default {
-
+  name:'HomeIndex'
 }
 </script>
 
---
+++ src/views/Video/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>视频详情页</h2>
+  <router-link to="/"></router-link>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
